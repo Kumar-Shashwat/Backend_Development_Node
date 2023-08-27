1e830c89248953602fd6c51f10a9ece0c2e5961a
Kumar Shashwat <kumarshashwa20@gamil.com>
2023-08-27 09:50:18 +0530

    first commit

---
+++ node-first-app/first-app.js
@@ -0,0 +1,5 @@
+// console.log("hello world! from node.js")
+
+const fs = require('fs');
+
+fs.writeFileSync("hello.txt", 'Hello World from the node.js'); // creating a new file with some added content.

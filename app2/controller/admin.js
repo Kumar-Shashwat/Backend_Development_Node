85c18825954d56c5b0bb32012ad2bf9eaf78517b
Kumar Shashwat <kumarshashwa20@gamil.com>
2023-09-16 22:39:56 +0530

    enhancing app2

---
+++ app2/controller/admin.js
@@ -0,0 +1,13 @@
+const Product = require('../models/product');
+
+exports.getAddProducts = (req, res, next) => {
+
+    res.render('admin/add-product', {title : 'add-product' , path : 'admin/add-product'});
+};
+
+exports.postAddProduct = (req, res, next) => {
+
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/shop');
+};
--- app2/routes/admin.js
+++ app2/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const productsController = require('../controller/products');
+const productsController = require('../controller/admin');
 
 
 

1379e5a3ada65156be1600ac7e84890e8fac6b99
Kumar Shashwat <kumarshashwa20@gamil.com>
2023-09-16 23:18:23 +0530

    again enhancing the app

--- app2/controller/shop.js
+++ app2/controller/shop.js
@@ -6,3 +6,15 @@ exports.shopGallery = (req, res , next) => {
     res.render('shop/product-list', {prod : Product.fetchAll(), title : 'product-list', path : '/shop/product-list'});
     console.log(Product.fetchAll());
 };
+
+exports.cart = (req, res, next) => {
+    res.render('shop/cart', {title : 'cart', path : 'shop/cart'});
+};
+
+exports.cheakout = (req, res, next) => {
+    res.render('shop/cheakout', {title : 'cheakout', path : 'shop/cheakout'});
+};
+
+exports.productDetials = (req, res, next) => {
+    res.render('shop/product-details', {title : 'product-detials', path : 'shop/product-detials'});
+};
--- app2/routes/shop.js
+++ app2/routes/shop.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const { shopGallery } = require('../controller/shop');
+const shopController = require('../controller/shop');
 
-router.get('/product-list', shopGallery);
+router.get('/product-list', shopController.shopGallery);
+router.get('/cart', shopController.cart);
+router.get('/cheakout', shopController.cheakout );
+router.get('/product-details', shopController.productDetials);
+
 
 module.exports = router;
